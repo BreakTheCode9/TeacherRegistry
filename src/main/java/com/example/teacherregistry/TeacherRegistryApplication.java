67c0dca7e40f612650f6eba8e8080c92dfb4a3e2
BreakTheCode9 <bankaleayoyimika@gmail.com>
2023-10-11 16:59:32 +0100

    Teacher Project

---
+++ src/main/java/com/example/teacherregistry/TeacherRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.teacherregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherRegistryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teacherregistry/TeacherRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teacherregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
